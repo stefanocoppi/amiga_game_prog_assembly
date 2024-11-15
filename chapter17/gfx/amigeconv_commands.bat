d52f266559709a93d7561c50bedd3bad987e1990
MKinght73 <stefano.coppi@gmail.com>
2024-11-15 17:19:05 +0100

    Added chapter 17

---
+++ chapter17/gfx/amigeconv_commands.bat
@@ -0,0 +1,14 @@
+@echo off
+amigeconv.exe -f bitplane -d 8 shooter_tiles.png shooter_tiles.raw
+amigeconv.exe -f palette -p pal8 -c 256 -x shooter_tiles.png  shooter.pal
+amigeconv.exe -f bitplane -d 8 ship.png ship.raw
+amigeconv.exe -f bitplane -m -d 1 ship.png ship.mask
+amigeconv.exe -f bitplane -d 8 .\ship_engine.png ship_engine.raw
+amigeconv.exe -f bitplane -m -d 1 .\ship_engine.png ship_engine.mask
+amigeconv.exe -f bitplane -d 8 .\enemies.png enemies.raw
+amigeconv.exe -f bitplane -m -d 1 .\enemies.png enemies.mask
+amigeconv.exe -f bitplane -d 8 .\ship_shots.png ship_shots.raw
+amigeconv.exe -f bitplane -m -d 1 .\ship_shots.png ship_shots.mask
+amigeconv.exe -f bitplane -d 8 .\enemy_shots.png enemy_shots.raw
+amigeconv.exe -f bitplane -m -d 1 .\enemy_shots.png enemy_shots.mask
+
