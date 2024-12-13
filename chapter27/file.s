f51a43889d2fbb74bc1f6396155e95cd8a1e67d7
MKinght73 <stefano.coppi@gmail.com>
2024-12-13 17:30:12 +0100

    added chapter 27 code

---
+++ chapter27/file.s
@@ -0,0 +1,60 @@
+;****************************************************************
+; File loading
+;
+; (c) 2024 Stefano Coppi
+;****************************************************************
+
+             xref       dos_base
+
+
+;****************************************************************
+; CONSTANTS
+;****************************************************************
+MODE_OLDFILE equ $3ed
+LVOOpen      equ -$1e
+LVORead      equ -$2A
+LVOClose     equ -$24
+
+
+;****************************************************************
+; VARIABLES
+;****************************************************************
+             SECTION    code_section,CODE
+
+file_handle  dc.l       0
+
+;****************************************************************
+; SUBROUTINES
+;****************************************************************
+
+
+;****************************************************************
+; Loads a file from disk using AmigaDOS.
+;
+; parameters:
+; d1.l - pointer to filename (specify the entire path)
+; d2.l - address of buffer where to load the file content
+; d3.l - file length in bytes
+
+;****************************************************************
+             xdef       load_file
+load_file:
+             movem.l    d0-a6,-(sp)
+
+             move.l     d2,d4                ; makes a copy
+             move.l     #MODE_OLDFILE,d2     ; read mode
+             move.l     dos_base,a6
+             jsr        LVOOpen(a6)          ; opens file
+             move.l     d0,file_handle       ; saves file handle
+             beq        .return              ; if handle = 0 there is an error
+
+             move.l     d0,d1
+             move.l     d4,d2
+             jsr        LVORead(a6)          ; reads the file
+
+             move.l     file_handle,d1
+             ;move.l     dos_base,a6
+             jsr        LVOClose(a6)         ; closes the file
+.return:
+             movem.l    (sp)+,d0-a6
+             rts
