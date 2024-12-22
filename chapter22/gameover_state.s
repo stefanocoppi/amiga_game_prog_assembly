6b567f5249014526ed18245175152602384853cd
MKinght73 <stefano.coppi@gmail.com>
2024-12-22 17:25:46 +0100

    Updated chapter 22 code

---
+++ chapter22/gameover_state.s
@@ -0,0 +1,188 @@
+;****************************************************************
+; Game over state
+;
+; (c) 2024 Stefano Coppi
+;****************************************************************
+
+                include    "game_state.i"
+                include    "playfield.i"
+                include    "bob.i"
+                include    "sound.i"
+
+                xref       swap_buffers
+                xref       scroll_background
+                xref       plship_update
+                xref       ship_fire_shot
+                xref       ship_shots_update
+                xref       enemy_shots_update
+                xref       enemies_activate
+                xref       enemies_update
+                xref       check_coll_shots_enemies
+                xref       check_coll_shots_plship
+                xref       check_coll_enemy_plship
+                xref       check_coll_plship_map
+                xref       erase_bgnds
+                xref       enemies_draw
+                xref       plship_draw
+                xref       ship_shots_draw
+                xref       enemy_shots_draw
+                xref       bplpointers1
+                xref       playfield1
+                xref       init_bplpointers
+                xref       bplpointers2
+                xref       playfield2a
+                xref       map_ptr
+                xref       init_background
+                xref       bgnd_x
+                xref       camera_x
+                xref       plship_init
+                xref       init_hud
+                xref       game_state
+                xref       shots_init
+                xref       draw_buffer
+                xref       change_gamestate
+                xref       play_sfx
+                xref       play_sample
+  
+                xdef       init_gameover_state
+                xdef       update_gameover_state
+
+FLASH_DURATION equ 6
+STATE_DURATION equ 50*6                                       ; 6 seconds
+
+;****************************************************************
+; GRAPHICS DATA in chip ram
+;****************************************************************
+                SECTION    graphics_data,DATA_C
+gameover_gfx    incbin     "gfx/game_over.raw"
+gameover_mask   incbin     "gfx/game_over.mask"
+
+
+;****************************************************************
+; VARIABLES
+;****************************************************************
+                SECTION    code_section,CODE
+
+gameover_text   dc.w       CLIP_LEFT+70                       ; x position
+                dc.w       90                                 ; y position
+                dc.w       1                                  ; speed
+                dc.w       176                                ; width
+                dc.w       22                                 ; height  
+                dc.w       0                                  ; spritesheet column of the bob
+                dc.w       0                                  ; spritesheet row of the bob
+                dc.w       176                                ; spritesheet width in pixels
+                dc.w       44                                 ; spritesheet height in pixels
+                dc.l       gameover_gfx                       ; image data address
+                dc.l       gameover_mask 
+
+flash_timer     dc.w       0
+gameover_timer  dc.w       0                                  ; measures the permanence in this state
+
+
+;****************************************************************
+; Initializes the GAMEOVER game state.
+;****************************************************************
+init_gameover_state:
+                movem.l    d0-a6,-(sp)
+
+                move.w     #STATE_DURATION,gameover_timer
+                move.w     #FLASH_DURATION,flash_timer
+                lea        gameover_text,a0
+                move.w     #0,bob.ssheet_r(a0)
+
+; changes the game state to GAMEOVER
+                move.w     #GAME_STATE_GAMEOVER,game_state
+
+; plays sound fx
+                move.w     #SFX_ID_GAMEOVER,d0
+                clr.w      d1                                 ; no loop
+                jsr        play_sfx
+                ;jsr        play_sample
+                
+
+                movem.l    (sp)+,d0-a6
+                rts
+
+
+;****************************************************************
+; Updates the GAMEOVER game state.
+;****************************************************************
+update_gameover_state:
+                movem.l    d0-a6,-(sp)
+
+                jsr        swap_buffers                       ; swaps draw and view buffers for implementing double buffering
+
+                jsr        scroll_background                  ; scrolls tilemap toward left
+
+                jsr        plship_update                      ; updates player's ship state
+                jsr        ship_fire_shot                     ; fires shots from player's ship
+                jsr        ship_shots_update                  ; updates player's ship shots state
+                jsr        enemy_shots_update                 ; updates enemy shots state
+    
+                jsr        enemies_activate                   ; activates enemies based on their position on the map 
+                jsr        enemies_update                     ; updates enemies state
+
+                jsr        check_coll_shots_enemies           ; checks collisions between player's shots and enemies
+                jsr        check_coll_shots_plship            ; checks collisions between enemy shots and player's ship
+                jsr        check_coll_enemy_plship            ; checks collisions between enemy and player's ship
+                jsr        check_coll_plship_map              ; checks collisions between player's ship and tilemap
+    
+                jsr        erase_bgnds                        ; erases bobs backgrounds
+                    
+                jsr        enemies_draw                       ; draws enemies
+                jsr        plship_draw                        ; draws player's ship
+                jsr        ship_shots_draw                    ; draws player's ship shots
+                jsr        enemy_shots_draw                   ; draws enemy shots
+
+                jsr        update_gameover_text
+                jsr        draw_gameover_text
+
+                movem.l    (sp)+,d0-a6
+                rts
+
+
+;****************************************************************
+; Draws the gameover text.
+;****************************************************************
+draw_gameover_text:
+                movem.l    d0-a6,-(sp)
+
+                lea        gameover_text,a3       
+                move.l     draw_buffer,a2
+                bsr        draw_bob
+
+.return:
+                movem.l    (sp)+,d0-a6
+                rts
+
+
+;****************************************************************
+; Updates the gameover text, making it flash.
+;****************************************************************
+update_gameover_text:
+                movem.l    d0-a6,-(sp)     
+               
+                sub.w      #1,gameover_timer
+                beq        .change_state
+                bra        .decr_flash_timer
+.change_state:
+                move.w     #GAME_STATE_TITLESCREEN,d0
+                jsr        change_gamestate
+.decr_flash_timer:
+                sub.w      #1,flash_timer
+                beq        .change_frame
+                bra        .return
+
+.change_frame:
+                move.w     #FLASH_DURATION,flash_timer
+                lea        gameover_text,a0
+                add.w      #1,bob.ssheet_r(a0)
+                cmp.w      #2,bob.ssheet_r(a0)
+                bge        .reset_animation
+                bra        .return
+.reset_animation:
+                clr.w      bob.ssheet_r(a0)       
+
+.return:
+                movem.l    (sp)+,d0-a6
+                rts
---
+++ chapter22/takeover.s
@@ -0,0 +1,97 @@
+;****************************************************************
+; Takes and releases control of Amiga hardware.
+;
+; (c) 2024 Stefano Coppi
+;****************************************************************
+          
+             incdir     "include"
+             include    "hw.i"
+             include    "funcdef.i"
+             include    "exec/exec_lib.i"
+             include    "graphics/graphics_lib.i"
+             include    "takeover.i"
+ 
+             xdef       take_system,release_system
+             xref       copperlist
+
+
+;************************************************************************
+; VARIABLES
+;************************************************************************
+gfx_name     dc.b       "graphics.library",0,0        ; string containing the name of graphics.library
+gfx_base     dc.l       0                             ; base address of graphics.library
+old_dma      dc.w       0                             ; saved state of DMACON
+sys_coplist  dc.l       0                             ; address of system copperlist                
+old_intena   dc.w       0                             ; saved value of INTENA
+old_intreq   dc.w       0                             ; saved value of INTREQ
+old_adkcon   dc.w       0                             ; saved value of ADKCON
+
+
+;************************************************************************
+; Takes full control of Amiga hardware,
+; disabling the O.S. in a controlled way.
+;************************************************************************
+take_system:
+             move.l     ExecBase,a6                   ; base address of Exec
+             jsr        _LVOForbid(a6)                ; disables O.S. multitasking
+             jsr        _LVODisable(a6)               ; disables O.S. interrupts
+
+             lea        gfx_name,a1                   ; OpenLibrary takes 1 parameter: library name in a1
+             jsr        _LVOOldOpenLibrary(a6)        ; opens graphics.library
+             move.l     d0,gfx_base                   ; saves base address of graphics.library in a variable
+            
+             move.l     d0,a6                         ; gfx base                   
+             move.l     $26(a6),sys_coplist           ; saves system copperlist address
+             
+             jsr        _LVOOwnBlitter(a6)            ; takes the Blitter exclusive
+
+             lea        CUSTOM,a5                     ; a5 will always contain CUSTOM chips base address $dff000
+          
+             move.w     DMACONR(a5),old_dma           ; saves state of DMA channels in a variable
+             move.w     #$7fff,DMACON(a5)             ; disables all DMA channels
+             move.w     #DMASET,DMACON(a5)            ; sets only dma channels that we will use
+
+             move.w     INTENAR(a5),old_intena        ; save interrupts state
+             move.w     INTREQR(a5),old_intreq
+             move.w     ADKCONR(a5),old_adkcon        ; save ADKCON
+             move.w     #$7fff,INTENA(a5)             ; disable all interrupts
+             move.w     #$7fff,INTREQ(a5)
+
+            ;  move.l     #copperlist,COP1LC(a5)        ; sets our copperlist address into Copper
+            ;  move.w     d0,COPJMP1(a5)                ; reset Copper PC to the beginning of our copperlist       
+
+             move.w     #0,FMODE(a5)                  ; sets 16 bit FMODE
+             move.w     #$c00,BPLCON3(a5)             ; sets default value                       
+             move.w     #$11,BPLCON4(a5)              ; sets default value
+
+             rts
+
+
+;************************************************************************
+; Releases the hardware control to the O.S.
+;************************************************************************
+release_system:
+             move.l     sys_coplist,COP1LC(a5)        ; restores the system copperlist
+             move.w     d0,COPJMP1(a5)                ; starts the system copperlist 
+
+             or.w       #$8000,old_dma                ; sets bit 15
+             move.w     old_dma,DMACON(a5)            ; restores saved DMA state
+
+             move.w     #$7fff,INTENA(a5)             ; disable all interrupts
+             move.w     #$7fff,INTREQ(a5)
+             move.w     #$7fff,ADKCON(a5)             ; clears ADKCON
+             or.w       #$8000,old_intena             ; sets bit 15
+             or.w       #$8000,old_intreq
+             or.w       #$8000,old_adkcon
+             move.w     old_intena,INTENA(a5)         ; restores saved interrupts state
+             move.w     old_intreq,INTREQ(a5)
+             move.w     old_adkcon,ADKCON(a5)         ; restores old value of ADKCON
+             
+             move.l     gfx_base,a6
+             jsr        _LVODisownBlitter(a6)         ; release Blitter ownership
+             move.l     ExecBase,a6                   ; base address of Exec
+             jsr        _LVOPermit(a6)                ; enables O.S. multitasking
+             jsr        _LVOEnable(a6)                ; enables O.S. interrupts
+             move.l     gfx_base,a1                   ; base address of graphics.library in a1
+             jsr        _LVOCloseLibrary(a6)          ; closes graphics.library
+             rts
