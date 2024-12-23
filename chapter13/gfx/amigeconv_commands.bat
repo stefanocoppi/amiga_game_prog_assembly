976f07de6aa6304978b0eee4a36e03f403d0bdd0
MKinght73 <stefano.coppi@gmail.com>
2024-12-23 18:16:07 +0100

    Updated book chapter 5

--- chapter13/gfx/amigeconv_commands.bat
+++ chapter13/gfx/amigeconv_commands.bat
@@ -4,7 +4,7 @@ amigeconv.exe -f bitplane -m -d 1 .\shooter_tiles_16.png shooter_tiles_16.mask
 amigeconv.exe -f palette -p pal4 -c 16 -x -n .\shooter_tiles_16.png shooter_tiles_16.pal
 amigeconv.exe -f bitplane -d 4 .\ship.png ship.raw
 amigeconv.exe -f bitplane -m -d 1 .\ship.png ship.mask
-::amigeconv.exe -f palette -p pal4 -c 16 -x -n .\ship.png pf2_palette.pal
+amigeconv.exe -f palette -p pal4 -c 16 -x -n .\ship.png pf2_palette.pal
 amigeconv.exe -f bitplane -d 4 .\ship_engine.png ship_engine.raw
 amigeconv.exe -f bitplane -m -d 1 .\ship_engine.png ship_engine.mask
 
